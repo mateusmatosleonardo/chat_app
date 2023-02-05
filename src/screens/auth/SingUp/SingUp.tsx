2991c7732029b1ad20ce30aa9b7451509921d060
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-05 16:08:59 -0300

    feat: starting with the registration screen

--- src/screens/auth/SingUp/SingUp.tsx
+++ src/screens/auth/SingUp/SingUp.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input } from '../../../components/Input/Input';
+import { style } from '../../../components/Input/styles';
 import * as S from './styles';
 
+import Chat from '../../../assets/chat.json';
+import LottieView from 'lottie-react-native';
+
 export function SingUp() {
+
+  const [showPassword, setShowPassword] = useState(true);
+
   return (
     <S.Container>
-
+      <LottieView
+        source={Chat}
+        style={{ width: 250, height: 250, marginBottom: 70 }}
+        autoPlay={false}
+        loop={false}
+      />
+      <S.Form>
+        <Input
+          hasIcon={true}
+          placeholder='Usuário'
+          style={style.input}
+        />
+        <Input
+          hasIcon={false}
+          placeholder='E-mail'
+          style={style.input}
+        />
+        <Input
+          hasIcon={true}
+          securityIcon={true}
+          placeholder='Senha'
+          style={style.input}
+          secureTextEntry={showPassword}
+          visiblePassword={showPassword}
+          onPress={() => setShowPassword(!showPassword)}
+        />
+      </S.Form>
     </S.Container>
   );
 }
