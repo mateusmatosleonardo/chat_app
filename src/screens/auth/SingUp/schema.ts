b552720ce8950fb885c44a0895e9e0c8ec79b852
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-05 22:42:22 -0300

    chore: changing values to the schema validate

--- src/screens/auth/SingUp/schema.ts
+++ src/screens/auth/SingUp/schema.ts
@@ -1,10 +1,10 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  username: yup.string().required("Informe seu nome de usuário!"),
-  email: yup.string().email("E-mail inválido").required("Informe seu e-mail!"),
+  username: yup.string().required("Informe um nome de usuário!"),
+  email: yup.string().email("E-mail inválido").required("Informe um e-mail!"),
   password: yup
     .string()
-    .min(8, "A senha deve possui no mínimo 8 caracteres")
-    .required("Informe sua senha!"),
+    .min(8, "A senha deve possui no mínimo 8 caracteres!")
+    .required("Informe uma senha!"),
 });
