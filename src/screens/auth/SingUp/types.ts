2558dc2c86ec91236a845f8e31f8d73f2143b73c
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-09 08:37:33 -0300

    :pencil2: changing directory name

--- src/screens/auth/SingUp/types.ts
+++
@@ -1,7 +0,0 @@
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParamsList } from "../../../routes/types";
-
-export type singUpScreenProp = NativeStackNavigationProp<
-  RootStackParamsList,
-  "SignUp"
->;
