b30cd5333a8030da0001361ad1756ac292a4561e
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-09 09:48:07 -0300

    :lipstick: creating style of search

---
+++ src/components/Search/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components/native";
+import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { theme } from "../../theme/theme";
+
+export const Search = styled.View`
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  border-radius: 12px;
+  margin-top: 12px;
+  background-color: ${({ theme }) => theme.colors.WHITE_SECONDARY};
+`;
+
+export const style = StyleSheet.create({
+  input: {
+    flex: 1,
+    height: 45,
+    marginHorizontal: 8,
+    fontFamily: theme.fonts.Poppins_Regular,
+    fontSize: RFValue(15),
+    letterSpacing: RFValue(0.2),
+    color: "#010101",
+  },
+});
