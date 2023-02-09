a9ce16ae6259a867039d0ede638856ad288ae1c8
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-09 12:50:27 -0300

    :lipstick: creating style for the homepage

---
+++ src/screens/app/Home/styles.ts
@@ -0,0 +1,28 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 22px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.Poppins_SemiBold};
+  font-size: ${RFValue(30)}px;
+  color: ${({ theme }) => theme.colors.BLACK};
+  padding: 6px 0px;
+`;
+
+export const Pressable = styled.Pressable``;
+
+export const WrapperSearch = styled.View`
+  width: 100%;
+  padding: 0px 22px;
+`;
