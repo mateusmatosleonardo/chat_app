d2e020757e42a447f9054c99cee5f6cafd942274
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-08 22:14:50 -0300

    :construction: importing fonts into the app

--- App.tsx
+++ App.tsx
@@ -3,11 +3,34 @@ import { Routes } from './src/routes/Routes';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/theme/theme';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import { AuthContextProvider } from './src/contexts/AuthContext/AuthProvider';
+
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="auto" />
-      <Routes />
+      <AuthContextProvider>
+        <StatusBar style="auto" />
+        <Routes />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
