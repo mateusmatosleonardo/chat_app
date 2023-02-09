ea1a69c32aa7c9a869f8d1dda43fd1039a160f6b
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-08 23:13:27 -0300

    :truck: adding screen of routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+// Auth Routes
+import { SignIn } from "../screens/auth/SignIn/SignIn";
+import { SignUp } from "../screens/auth/SingUp/SignUp";
+
+
+// App Routes
 import { Home } from "../screens/app/Home/Home";
 
 const Stack = createNativeStackNavigator();
@@ -9,9 +15,11 @@ const Stack = createNativeStackNavigator();
 export function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="SingIn" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
