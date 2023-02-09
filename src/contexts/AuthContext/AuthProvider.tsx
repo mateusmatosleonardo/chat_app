24a8947c214faeb601f23173d01b7ba05eaa30c3
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-08 23:25:43 -0300

    :construction: creating function signIn

--- src/contexts/AuthContext/AuthProvider.tsx
+++ src/contexts/AuthContext/AuthProvider.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { api } from "../../service/api";
 import { AuthContext } from "./AuthContext"
-import { AuthContextProps, IUser } from "./types"
+import { AuthContextProps, IUser, SignInForm } from "./types"
 
 export const AuthContextProvider = ({ children }: AuthContextProps) => {
 
   const [loading, setLoading] = useState(false);
 
-  async function handleSignUp(user: IUser) {
+  async function handleSignIn(user: SignInForm): Promise<any> {
+    setLoading(true);
+    try {
+      await api.post('/auth/login', user);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  async function handleSignUp(user: IUser): Promise<any> {
     setLoading(true);
     try {
       await api.post('/auth/register', user);
@@ -23,6 +34,7 @@ export const AuthContextProvider = ({ children }: AuthContextProps) => {
       value={{
         loading,
         setLoading,
+        handleSignIn,
         handleSignUp
       }}
     >
