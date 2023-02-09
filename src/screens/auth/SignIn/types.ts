2e29a9ae48facfa60a803d509b69255c251a82f5
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-09 08:28:55 -0300

    :label: creating signin route typing

---
+++ src/screens/auth/SignIn/types.ts
@@ -0,0 +1,7 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamsList } from "../../../routes/types";
+
+export type signInScreenProp = NativeStackNavigationProp<
+  RootStackParamsList,
+  "SignIn"
+>;
