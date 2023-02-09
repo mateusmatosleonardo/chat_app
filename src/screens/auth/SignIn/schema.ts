2028f2e696c685b02ce885b40c7bb06f3839a91e
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-09 08:34:18 -0300

    :safety_vest: creating schema for sign in validation

---
+++ src/screens/auth/SignIn/schema.ts
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+export const schema = yup.object({
+  username: yup.string().required("Informe seu nome de usuário!"),
+  password: yup
+    .string()
+    .min(8, "A senha deve possui no mínimo 8 caracteres!")
+    .required("Informe sua senha!"),
+});
