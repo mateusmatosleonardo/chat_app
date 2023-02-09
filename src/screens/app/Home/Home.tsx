222d1b1521b4e28a14baa28a6d5ba46ed26b67a5
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-09 15:17:23 -0300

    :lipstick: generating random color for background color

--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FlatList, ListRenderItemInfo } from 'react-native';
+
 import * as S from './styles';
 
 import Edit from '@expo/vector-icons/Feather';
@@ -6,7 +8,11 @@ import { Search } from '../../../components/Search/Search';
 import { Chat } from '../../../components/Chat/Chat';
 import { IChat } from '../../../components/Chat/types';
 
-import { FlatList, ListRenderItemInfo } from 'react-native';
+import Person1 from '../../../assets/person.png';
+
+let colors: string[] = ['#fcc09c', '#c0e4fe', '#ffecac', '#9c69bf'];
+
+let randomColors = colors[Math.floor(Math.random() * colors.length)];
 
 export function Home() {
 
@@ -18,32 +24,36 @@ export function Home() {
       username: 'Caio',
       lastMessage: 'Hi, my name is Caio',
       lastMessageTime: '12:35',
-      picture: require('../../../assets/person.png'),
-      status: 'on'
+      picture: Person1,
+      status: 'on',
+      colors: randomColors
     },
     {
       id: '19de3ed4-12d1-4600-be92-418994e70119',
       username: 'Lucas',
       lastMessage: 'Hi, my name is Caio',
       lastMessageTime: '12:35',
-      picture: require('../../../assets/person.png'),
-      status: 'off'
+      picture: Person1,
+      status: 'off',
+      colors: randomColors
     },
     {
       id: '19de3ed4-12d1-4600-be92-418994e70116',
       username: 'Roberta',
       lastMessage: 'Hi, my name is Caio',
       lastMessageTime: '12:35',
-      picture: require('../../../assets/person.png'),
-      status: 'on'
+      picture: Person1,
+      status: 'off',
+      colors: randomColors
     },
     {
       id: '19de3ed4-12d1-4600-be92-418994e70114',
       username: 'Thaís',
       lastMessage: 'Hi, my name is Caio',
       lastMessageTime: '12:35',
-      picture: require('../../../assets/person.png'),
-      status: 'on'
+      picture: Person1,
+      status: 'on',
+      colors: randomColors
     },
   ]);
 
