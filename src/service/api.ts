d432fb93e4f125ced4a082d81cc718f250e6a602
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-10 17:58:02 -0300

    :closed_lock_with_key: adding api_url from .env

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,11 +1,10 @@
+import { API_URL } from "@env";
 import axios, { InternalAxiosRequestConfig } from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: API_URL,
 });
-
-// :safety_vest: instantiating the interceptor for user validation
 
 api.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
   const token = await AsyncStorage.getItem("token");
