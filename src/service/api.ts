2fa7c3f7755d218304584428fe4f834475d18bc0
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-10 17:37:21 -0300

    :safety_vest: instantiating the interceptor for user validation

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,15 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
   baseURL: "http://localhost:3000",
+});
+
+// :safety_vest: instantiating the interceptor for user validation
+
+api.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
+  const token = await AsyncStorage.getItem("token");
+  if (!token) return config;
+  if (config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
