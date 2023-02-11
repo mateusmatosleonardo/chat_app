5d094796f7e7deebc57ceb11eb7967d38958d312
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-10 23:22:45 -0300

    :rocket: hook for simple user data request

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { api } from "../service/api";
+
+export function useFetch<T = unknown>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    api
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .finally(() => {
+        setIsFetching(false);
+      });
+  }, []);
+
+  return { data, isFetching };
+}
