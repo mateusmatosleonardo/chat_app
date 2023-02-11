2f151495443a843d84ded3ca09331bf9471defec
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-10 23:27:42 -0300

    :lipstick: adding style to the registration screen

---
+++ src/screens/auth/SignUp/styles.ts
@@ -0,0 +1,43 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.GRAY};
+`;
+
+export const Form = styled.Pressable`
+  width: 100%;
+  padding: 36px 30px 50px 30px;
+`;
+
+export const WrapperButtons = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 14px;
+`;
+
+export const TextError = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.Poppins_Medium};
+  font-size: ${RFValue(13)}px;
+  color: ${({ theme }) => theme.colors.RED};
+  padding-top: 5px;
+`;
+
+export const ActionButton = styled.Pressable`
+  width: 65%;
+`;
+
+export const EnterButton = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.Poppins_Medium};
+  font-size: ${RFValue(13)}px;
+  color: ${({ theme }) => theme.colors.BLACK};
+  padding: 6px 0px;
+  margin-top: 4px;
+`;
+
+export const ActivityIndicator = styled.ActivityIndicator``;
