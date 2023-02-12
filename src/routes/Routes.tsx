b7372aff5125a283ac5cacafcbd8217c092262c2
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-11 23:37:22 -0300

    :truck: changing routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// Auth Routes
 import { SignIn } from "../screens/auth/SignIn/SignIn";
-import { SignUp } from "../screens/auth/SingUp/SignUp";
-
-
-// App Routes
+import { SignUp } from "../screens/auth/SignUp/SignUp";
 import { Home } from "../screens/app/Home/Home";
 
 const Stack = createNativeStackNavigator();
 
 export function Routes() {
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SingIn" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
