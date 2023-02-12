80d0289912e04b2242e7f3ba9f28bd32d02c1781
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-11 23:36:26 -0300

    :bulb: commenting schema

--- src/screens/auth/SignIn/schema.ts
+++ src/screens/auth/SignIn/schema.ts
@@ -4,6 +4,6 @@ export const schema = yup.object({
   username: yup.string().required("Informe seu nome de usuário!"),
   password: yup
     .string()
-    .min(8, "A senha deve possui no mínimo 8 caracteres!")
+    // .min(8, "A senha deve possui no mínimo 8 caracteres!")
     .required("Informe sua senha!"),
 });
