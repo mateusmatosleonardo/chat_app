7bf56e7736dc3604f74ca9db2bf562776b079d81
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-11 23:50:52 -0300

    :lipstick finalizing sign in screen style

---
+++ src/screens/auth/SignIn/styles.ts
@@ -0,0 +1,29 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.GRAY};
+`;
+
+export const Form = styled.Pressable`
+  width: 100%;
+  padding: 36px 30px 50px 30px;
+`;
+
+export const WrapperButtons = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 14px;
+`;
+
+export const TextError = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.Poppins_Medium};
+  font-size: ${RFValue(13)}px;
+  color: ${({ theme }) => theme.colors.RED};
+  padding-top: 5px;
+`;
