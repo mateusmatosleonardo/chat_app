ecd9217c50cbeab7d604d01b702353ad603d40ba
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-13 18:41:08 -0300

    :construction: finalizing loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,6 +3,6 @@ import { Loading as Spinner } from './styles';
 
 export function Loading() {
   return (
-    <Spinner size={26} color='#010101' />
+    <Spinner size='small' color='#010101' />
   );
 }
