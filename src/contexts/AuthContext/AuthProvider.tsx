a6312b4a0004ed40e718d9cd08c7e7f1c25ee355
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-12 23:50:05 -0300

    :wastebasket: removing obsolete provider functions

--- src/contexts/AuthContext/AuthProvider.tsx
+++ src/contexts/AuthContext/AuthProvider.tsx
@@ -1,32 +1,17 @@
 import { useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { AuthContext } from "./AuthContext"
-import { AuthContextProps, IUser } from "./types"
+import { AuthContextProps } from "./types"
 
 export const AuthContextProvider = ({ children }: AuthContextProps) => {
 
   const [loading, setLoading] = useState(false);
-
-  const auth = useAuth();
-
-  async function handleSignUp(user: IUser): Promise<any> {
-    setLoading(true);
-    try {
-      const data = await auth.handleSignUp(user);
-      setLoading(false);
-      return data;
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
 
   return (
     <AuthContext.Provider
       value={{
         loading,
         setLoading,
-        handleSignUp
       }}
     >
       {children}
