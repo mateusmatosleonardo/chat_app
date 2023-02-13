cb38887244091f23b332a46e90559f8d5ba343c9
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-13 18:36:40 -0300

    :safety_vest: changing property names in sign in scheme

--- src/screens/auth/SignIn/schema.ts
+++ src/screens/auth/SignIn/schema.ts
@@ -1,9 +1,6 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  user: yup.string().required("Informe seu nome de usuário!"),
-  password: yup
-    .string()
-    // .min(8, "Sua senha deve possui no mínimo 8 caracteres!")
-    .required("Informe sua senha!"),
+  email: yup.string().required("Informe seu email!"),
+  password: yup.string().required("Informe sua senha!"),
 });
