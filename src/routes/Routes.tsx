e335e2106fb5f85043e316264a3fa086295f762c
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-12 23:48:46 -0300

    :truck: adding chat screen in route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SignIn } from "../screens/auth/SignIn/SignIn";
 import { SignUp } from "../screens/auth/SignUp/SignUp";
 import { Home } from "../screens/app/Home/Home";
+import { Chat } from "../screens/app/Chat/Chat";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export function Routes() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Chat" component={Chat} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
