20f2c5b0eb6480006f9beadd9c35a64d8150a5e2
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-12 23:47:41 -0300

    :label: creating type of route home

---
+++ src/screens/app/Home/types.ts
@@ -0,0 +1,7 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamsList } from "../../../routes/types";
+
+export type HomeScreenProp = NativeStackNavigationProp<
+  RootStackParamsList,
+  "Home"
+>;
