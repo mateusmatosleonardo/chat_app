1d6466ca1ff6cdeaa1031519a87275f7f48d6577
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-13 17:49:14 -0300

    :recycle: refactoring component chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,21 +3,22 @@ import * as S from './styles';
 
 import { IChat } from './types';
 
-import Person1 from '../../assets/person.png';
+import Person from '../../assets/person.png';
 
 const statusColor = {
   active: '#1eaf0a',
   inactive: '#c4c4c4'
 }
 
-export function Chat({ participant, lastMessage, lastMessageTime, status, colors }: IChat) {
+export function Chat({ status, participant, lastMessage, lastMessageTime, colors, onPress }: IChat) {
   return (
-    <S.Container>
+    <S.Container onPress={onPress}>
+
       <S.WrapperPicture
         backgroundPicture={colors}
       >
         <S.Picture
-          source={Person1}
+          source={Person}
           imageStyle={{
             borderRadius: 62 / 2,
             resizeMode: 'center'
@@ -30,7 +31,8 @@ export function Chat({ participant, lastMessage, lastMessageTime, status, colors
               shadowOffset: { width: 0, height: 1 },
               shadowOpacity: 0.2,
               shadowRadius: 2,
-            }} />
+            }}
+          />
         </S.Picture>
       </S.WrapperPicture>
       <S.WrapperMain>
@@ -38,7 +40,6 @@ export function Chat({ participant, lastMessage, lastMessageTime, status, colors
         <S.Wrapper>
           <S.Username>{participant}</S.Username>
           <S.LastMessageTime>{lastMessageTime}</S.LastMessageTime>
-
         </S.Wrapper>
 
         <S.WrapperLastMessage>
