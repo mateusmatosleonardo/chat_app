9f066b1aa034f0df4c0560144d3fe1ad6df248e2
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-13 22:29:46 -0300

    :wastebasket: deleting unused file

--- src/screens/app/Home/types.ts
+++
@@ -1,7 +0,0 @@
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParamsList } from "../../../routes/types";
-
-export type HomeScreenProp = NativeStackNavigationProp<
-  RootStackParamsList,
-  "Home"
->;
