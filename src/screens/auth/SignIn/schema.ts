353074e895313350d1cb80df0331fb1a421be85e
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-14 20:22:00 -0300

    :adhesive_bandage: changing sign in scheme

--- src/screens/auth/SignIn/schema.ts
+++ src/screens/auth/SignIn/schema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  email: yup.string().required("Informe seu email!"),
+  user: yup.string().required("Informe seu email!"),
   password: yup.string().required("Informe sua senha!"),
 });
