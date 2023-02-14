ffaf0dcb20079f099322ea41248adb4ad091872a
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-14 02:00:43 -0300

    :safety_vest: changing registration validation schema

--- src/screens/auth/SignUp/schema.ts
+++ src/screens/auth/SignUp/schema.ts
@@ -1,10 +1,7 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  username: yup.string().required("Informe um nome de usuário!"),
+  username: yup.string().required("Crie um nome de usuário!"),
   email: yup.string().email("E-mail inválido").required("Informe um e-mail!"),
-  password: yup
-    .string()
-    .min(8, "A senha deve possui no mínimo 8 caracteres!")
-    .required("Informe uma senha!"),
+  password: yup.string().required("Crie uma senha!"),
 });
