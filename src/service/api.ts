b253e41531693655d278558b07a890b39989aa96
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-15 20:47:14 -0300

    :poop: creating function to fetch token

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,13 +1,14 @@
 import { API_URL } from "@env";
+import { getToken } from "./userStorage";
 import axios, { InternalAxiosRequestConfig } from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
   baseURL: API_URL,
 });
 
 api.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
-  const token = await AsyncStorage.getItem("token");
+  const token: string = await getToken();
+  console.log(`Token -> ${token}`);
   if (!token) return config;
   if (config.headers) config.headers.Authorization = `Bearer ${token}`;
   return config;
