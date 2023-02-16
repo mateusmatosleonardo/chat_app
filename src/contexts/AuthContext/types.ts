de85e8adab4ca9eca53acdcd35712b9c287821ed
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-15 23:47:11 -0300

    :coffin: removing unused typing codes

--- src/contexts/AuthContext/types.ts
+++ src/contexts/AuthContext/types.ts
@@ -1,20 +1,8 @@
 import { ReactNode } from "react";
 
-export interface IUser {
-  email: string;
-  id: number;
-  name: string;
-}
-
-export interface SignInForm {
-  username: string;
-  password: string;
-}
-
 export interface IAuthContext {
   loading: boolean;
   setLoading: (loading: boolean) => void;
-  handleSignUp: (user: IUser) => Promise<any>;
 }
 
 export interface AuthContextProps {
