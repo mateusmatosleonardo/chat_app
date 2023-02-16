70c8213a304553aacab6725bccba3738caab7229
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-15 23:42:52 -0300

    :construction: home in progress

--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+
 import { FlatList, ListRenderItemInfo } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import * as S from './styles';
 
-import Edit from '@expo/vector-icons/Feather';
 import { Search } from '../../../components/Search/Search';
 import { Chat } from '../../../components/Chat/Chat';
-import { IChat } from '../../../components/Chat/types';
+import { Loading } from '../../../components/Loading/Loading';
 
-import Person1 from '../../../assets/person.png';
+import { IGetAllChats } from '../../../components/Chat/types';
+import { HomeScreenProp } from '../../../utils/globalTypes';
+import { IUser } from '../../../utils/globalTypes';
+
+import Edit from '@expo/vector-icons/Feather';
+import Icon from '@expo/vector-icons/Ionicons';
+
+import { useFetch } from '../../../hooks/useFetch';
 
 let colors: string[] = ['#fcc09c', '#c0e4fe', '#ffecac', '#9c69bf'];
 
@@ -18,57 +26,46 @@ export function Home() {
 
   const [search, setSearch] = useState('');
 
-  const [chats, setChats] = useState<IChat[]>([
-    {
-      id: '19de3ed4-12d1-4600-be92-418994e70118',
-      username: 'Caio',
-      lastMessage: 'Hi, my name is Caio',
-      lastMessageTime: '12:35',
-      picture: Person1,
-      status: 'on',
-      colors: randomColors
-    },
-    {
-      id: '19de3ed4-12d1-4600-be92-418994e70119',
-      username: 'Lucas',
-      lastMessage: 'Hi, my name is Caio',
-      lastMessageTime: '12:35',
-      picture: Person1,
-      status: 'off',
-      colors: randomColors
-    },
-    {
-      id: '19de3ed4-12d1-4600-be92-418994e70116',
-      username: 'Roberta',
-      lastMessage: 'Hi, my name is Caio',
-      lastMessageTime: '12:35',
-      picture: Person1,
-      status: 'off',
-      colors: randomColors
-    },
-    {
-      id: '19de3ed4-12d1-4600-be92-418994e70114',
-      username: 'Thaís',
-      lastMessage: 'Hi, my name is Caio',
-      lastMessageTime: '12:35',
-      picture: Person1,
-      status: 'on',
-      colors: randomColors
-    },
-  ]);
+  const navigation = useNavigation<HomeScreenProp>();
 
-  const filteredChat = search.length > 0
-    ? chats.filter(chat => chat.username.includes(search))
-    : [];
+  const { data: user } = useFetch<IUser>("/users/me");
+  const { data: chats, isFetching } = useFetch<IGetAllChats[]>("/chats");
 
-  function renderItem({ item }: ListRenderItemInfo<IChat>) {
-    return <Chat {...item} />
+  // ! integrar essa feature de pesquisa
+  // const filteredChat = search.length > 0
+  // ? chats?.filter(chat => chat.participants.map(({ participant }) => participant.username.includes(search)))
+  //   : [];
+
+  function renderItem({ item }: ListRenderItemInfo<IGetAllChats>) {
+    return <Chat
+      id={item.id}
+      participant={item.participants.find(({ participant }) => participant.id !== user!.id)?.participant.username || ''}
+      lastMessage={item.messages[0].body}
+      lastMessageTime={item.messages[0].createdAt.substring(11, 16)}
+      colors={randomColors}
+      onPress={() => navigation.navigate('Chat',
+        { id: item.id, participant: item.participants.find(({ participant }) => participant.id !== user!.id)?.participant.username || '' })}
+    />
+  }
+
+  function ListEmptyComponent() {
+    return (
+      <S.EmptyList>
+        <S.EmptyListMessage>Inicie uma conversa</S.EmptyListMessage>
+        <Icon
+          name='chatbubble-ellipses-outline'
+          color='#010101'
+          size={16}
+          style={{ marginLeft: 10 }}
+        />
+      </S.EmptyList>
+    )
   }
 
   return (
     <S.Container>
       <S.Header>
-        <S.Title>Chats</S.Title>
+        <S.Title>Conversas</S.Title>
         <S.Pressable>
           <Edit name='edit' size={22} color="#010101" />
         </S.Pressable>
@@ -78,19 +75,16 @@ export function Home() {
         <Search
           value={search}
           onChangeText={setSearch}
-          placeholder='Pesquisar conversar'
+          placeholder='Pesquisar conversas'
         />
       </S.WrapperSearch>
 
-      {search.length > 0 ? <FlatList
-        data={filteredChat}
-        keyExtractor={item => item.id}
-        renderItem={renderItem}
-      /> : <FlatList
+      {user && !isFetching ? <FlatList
         data={chats}
         keyExtractor={item => item.id}
         renderItem={renderItem}
-      />}
+        ListEmptyComponent={ListEmptyComponent}
+      /> : <Loading />}
 
     </S.Container>
   );
